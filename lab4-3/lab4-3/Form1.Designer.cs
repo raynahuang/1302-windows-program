dce39e190a9667db6d25a6d4e095f0941d2f8a96
Rayna Huang <raynainca190217@gmail.com>
2023-05-24 12:11:32 -0700

    change lab name

--- lab4-3/lab4-3/Form1.Designer.cs
+++ lab4-3/lab4-3/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.LightOn;
-            pictureBox1.Location = new Point(24, 12);
+            pictureBox1.Location = new Point(61, 12);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(258, 225);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -54,14 +54,14 @@
             label1.AutoSize = true;
             label1.Location = new Point(309, 280);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(41, 15);
             label1.TabIndex = 2;
-            label1.Text = "label1";
+            label1.Text = "switch";
             label1.Click += label1_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(262, 319);
+            button1.Location = new Point(244, 319);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
@@ -71,7 +71,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(355, 319);
+            button2.Location = new Point(341, 319);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 4;
@@ -82,9 +82,9 @@
             // 
             pictureBox2.BackgroundImageLayout = ImageLayout.Center;
             pictureBox2.Image = Properties.Resources.LightOff;
-            pictureBox2.Location = new Point(355, 12);
+            pictureBox2.Location = new Point(325, 12);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(244, 225);
+            pictureBox2.Size = new Size(258, 225);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 5;
             pictureBox2.TabStop = false;
--- lab4-3/lab4-3/Form1.cs
+++ lab4-3/lab4-3/Form1.cs
@@ -25,11 +25,11 @@ namespace lab4_3
         private void button1_Click(object sender, EventArgs e)
         {
             lightON = !lightON;
-           
+            flip();
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void flip()
         {
             if (lightON)
             {
@@ -42,10 +42,36 @@ namespace lab4_3
                 pictureBox2.Visible = true;
             }
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int myVar = 99;
+            setToZero(ref myVar);
+            MessageBox.Show(myVar.ToString()); //99
+        }
+
+        private void setToZero(ref int number)
+        {
+            number = 0;
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+            if (lightON)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+        }*/
     }
 }
-    }
-}
+
+
 
 /*
 namespace WinFormsApp5
